83ee7109aa34d19490baeb4d34b3b52492954eca
jesusmarzor <jesusmarzor28@gmail.com>
2023-06-18 18:11:17 +0200

    Fix: footer responsive

--- components/Footer/ExternalLinks.tsx
+++ components/Footer/ExternalLinks.tsx
@@ -6,7 +6,7 @@ export const ExternalLinks = () => {
     const footerLinks = appLinks.filter( ({position}) => position == "footer")
     return(
         <nav>
-            <ul className="flex justify-center items-center flex-wrap gap-56 text-xs">
+            <ul className="flex justify-center items-center flex-wrap gap-6 xl:gap-56 text-xs">
                 {
                     footerLinks.map( ({id, name, route}) => {
                         return (
--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -4,7 +4,7 @@ import { SocialNetworks } from "./SocialNetworks"
 export const Footer = () => {
     return(
         <footer className="bg-lightBlue mt-auto">
-            <div className="flex justify-between items-center flex-wrap py-8 px-10 max-w-7xl mx-auto">
+            <div className="flex justify-center items-center flex-wrap py-8 px-10 max-w-7xl gap-6 mx-auto xl:justify-between">
                 <ExternalLinks/>
                 <SocialNetworks/>
             </div>
