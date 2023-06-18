6a87f81cf9dc392093eba8b5d3c4e6bca6b98175
jesusmarzor <jesusmarzor28@gmail.com>
2023-06-18 21:15:16 +0200

    Fix: search design

--- components/ui/Input.tsx
+++ components/ui/Input.tsx
@@ -8,8 +8,8 @@ interface props {
 export const Input: React.FC<props> = ({title, changeText}) => {
     return(
         <div className="relative flex h-12 max-w-sm m-8">
-            <SearchIcon classesName="absolute top-0 bottom-0 my-auto left-4" width="24" height="24"/>
-            <input className="pl-16 w-full bg-lightBlue placeholder-violetBlue"  onChange={e => changeText(e)} placeholder={title}/>
+            <SearchIcon classesName="absolute top-0 bottom-0 my-auto left-10" width="16" height="16"/>
+            <input className="pl-20 w-full bg-lightBlue placeholder-violetBlue text-sm"  onChange={e => changeText(e)} placeholder={title}/>
         </div>
     )
 }
